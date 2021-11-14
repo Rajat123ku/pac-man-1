0b5121f8de832d65ae160cbca8564ce0633bf920
Rajat123ku <76685895+Rajat123ku@users.noreply.github.com>
2021-11-14 11:54:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13, wall14, wall15, wall16, wall17, wall18,wall19, wall20, wall21, wall22, wall23, wall24, wall25 ;
+
+function preload(){
+
+
+}
+
+function setup(){
+  createCanvas(600,600)
+  wall1 = createSprite(150,30,250,5);
+  wall2 = createSprite(450,30,250,5);
+  wall3 = createSprite(272,15,5,25);
+  wall4 = createSprite(328,15,5,25);
+  wall5 = createSprite(150,570,250,5);
+  wall6 = createSprite(450,570,250,5);
+  wall7 = createSprite(272,585,5,25);
+  wall8 = createSprite(328,585,5,25);
+  wall9 = createSprite(28,153,5,250);
+  wall10 = createSprite(28,447,5,250);
+  wall11 = createSprite(14,275,25,5);
+  wall12 = createSprite(14,325,25,5);
+  wall13 = createSprite(572,153,5,250);
+  wall14 = createSprite(572,447,5,250);
+  wall15 = createSprite(586,275,25,5);
+  wall16 = createSprite(586,325,25,5);
+  wall17 = createSprite(180,100,160,5);
+  wall18 = createSprite(257,156,5,110);
+  wall19 = createSprite(103,115,5,30);
+  wall20 = createSprite(305,210,100,5);
+  wall21 = createSprite(352,177,5,70);
+  wall22 = createSprite(350,140,90,5);
+  wall23 = createSprite(308,157,5,30);
+  wall24 = createSprite(392,197,5,110);
+  wall25 = createSprite(80,395,5,355);
+
+
+
+  //Shape Color
+  wall1.shapeColor = "blue"
+  wall2.shapeColor = "blue"
+  wall3.shapeColor = "blue"
+  wall4.shapeColor = "blue"
+  wall5.shapeColor = "blue"
+  wall6.shapeColor = "blue"
+  wall7.shapeColor = "blue"
+  wall8.shapeColor = "blue"
+  wall9.shapeColor = "blue"
+  wall10.shapeColor = "blue"
+  wall11.shapeColor = "blue"
+  wall12.shapeColor = "blue"
+  wall13.shapeColor = "blue"
+  wall14.shapeColor = "blue"
+  wall15.shapeColor = "blue"
+  wall16.shapeColor = "blue"
+  wall17.shapeColor = "blue"
+  wall18.shapeColor = "blue"
+  wall19.shapeColor = "blue"
+  wall20.shapeColor = "blue"
+  wall21.shapeColor = "blue"
+  wall22.shapeColor = "blue"
+  wall23.shapeColor = "blue"
+  wall24.shapeColor = "blue"
+  wall25.shapeColor = "blue"
+
+
+
+}
+
+function draw(){
+  background("black")
+
+
+
+ drawSprites(); 
+}
